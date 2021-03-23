6d096943d081a6c15a8382d7aad11da8413a8867
Dawid Kubicki <dawid@pluscode.io>
2021-03-23 12:31:12 +0100

    another example of state

--- State/State/ContentView.swift
+++ State/State/ContentView.swift
@@ -11,18 +11,23 @@ struct ContentView: View {
     
     //MARK: - Variables
     
-    @State var name: String = "John"
+    @State var tasks = [Task]()
+    
+    private func addTask() {
+        self.tasks.append(Task(name: "Wash the car"))
+    }
     
     var body: some View {
-        VStack {
-            Text(name)
-                .padding()
-                .font(.largeTitle)
-            Button(action: {
-                self.name = "Mary"
-            }) {
-                Text("Change Name")
+        List {
+            
+            Button(action: addTask) {
+                Text("Add Task")
             }
+            
+            ForEach(tasks) { task in
+                Text(task.name)
+            }
+            
         }
     }
 }
---
+++ State/State/Model/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  State
+//
+//  Created by Dawid Kubicki on 23/03/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct Task:Identifiable {
+    let id = UUID()
+    let name: String
+}
